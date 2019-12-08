b79bec0757dc6b869c0d566c55b9f8b592a22574
yq_mc <573380218@qq.com>
2019-12-08 04:21:06 -0500

    add

--- writer.py
+++ writer.py
@@ -13,10 +13,10 @@ class Writer:
         :param mouse: mouse object
         :param alpha: value of alpha
         :param gamma:  value of gamma
-        :param points: coordinates of the image on the cv windows
+        :param points: coordinates of the image on the opencv windows
         :param flag: initial flag
         :param prc_img_size:  process image size
-        :param lower: lower bound of the coloe
+        :param lower: lower bound of the color
         :param upper: upper bound of the color
         :param points_cnt: counter of points
         '''
@@ -36,12 +36,12 @@ class Writer:
         self.points_cnt = points_cnt
 
     # gamma transformation
-    def adjust_gamma(self, image, gamma):
+    def gamma_adjustment(self, image, gamma):
         '''
-        adjust value of gamma
-        :param image:
-        :param gamma:
-        :return:
+        adjust the gamma value of an image
+        :param image: input image
+        :param gamma: value of gamma
+        :return: new image
         '''
         invGamma = 1.0 / gamma
         table = np.array([((i / 255.0) ** invGamma) * 255
@@ -50,16 +50,27 @@ class Writer:
         return cv2.LUT(image, table)
     
     # record the coordinates of the points in the window
-    def draw_circle(self,event,x,y,flags,param):
-
+    def draw_points(self,event,x,y,flags,param):
+        '''
+        call-back function to draw points on the opencv window
+        :param event: event
+        :param x: x
+        :param y: y
+        :param flags: flags
+        :param param: param
+        :return: none
+        '''
         if event == cv2.EVENT_LBUTTONDOWN:
                 cv2.circle(self.img,(x,y),5,(0,255,0),-1)
                 self.points[self.points_cnt] = (x,y)
                 #print(pointIndex)
                 self.points_cnt = self.points_cnt + 1
 
-    # constantly show the image img
-    def show_window(self):                    
+    def window_points(self):
+        '''
+        find four points on the opencv window
+        :return: none
+        '''
         while True:
                 #print(pts,pointIndex-1)
                 cv2.imshow('img', self.img)
@@ -72,31 +83,40 @@ class Writer:
                     break
     
     # perspective transformation
-    # same object's position in different coordinates
-    def get_persp(self, image,points):
+    def perspetive_transform(self, image,points):
+        '''
+        standard opencv perspective transformation: same object's position in different coordinates
+        :param image: input image
+        :param points: input coordinates of an image
+        :return: new image
+        '''
         in_points = np.float32(points)
         Map = cv2.getPerspectiveTransform(in_points,self.out_points)
         warped = cv2.warpPerspective(image, Map, (self.prc_img_size[1], self.prc_img_size[0]))
         return warped
     
     def write(self):
+        '''
+        main function used for writer
+        :return: none
+        '''
         cv2.namedWindow('img')
         # mouse callback functions
-        cv2.setMouseCallback('img',self.draw_circle)
+        cv2.setMouseCallback('img',self.draw_points)
         print('Please points at: Top Left, Top Right, Bottom Left, Bottom Right')
 
         # constantly show the image img unless there are four points detected
-        self.show_window()
+        self.window_points()
         #cv2.destroyAllWindows()
 
         while True:
             # capture the frame by the camera
             _, frame = self.cap.read()
-            warped = self.get_persp(frame, self.points)
+            warped = self.perspetive_transform(frame, self.points)
 
             blurred = cv2.GaussianBlur(warped, (9, 9), 0)
             
-            adjusted = self.adjust_gamma(blurred, self.gamma)
+            adjusted = self.gamma_adjustment(blurred, self.gamma)
             
             hsv = cv2.cvtColor(adjusted,cv2.COLOR_BGR2HSV)
             mask = cv2.inRange(hsv, self.lower, self.upper)
@@ -105,18 +125,18 @@ class Writer:
             ret, otsu = cv2.threshold(mask,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
             # find contours from the previous image
-            cnts = cv2.findContours(otsu.copy(), cv2.RETR_EXTERNAL,
+            contour_num = cv2.findContours(otsu.copy(), cv2.RETR_EXTERNAL,
                 cv2.CHAIN_APPROX_SIMPLE)[-2]
             center = None
 
             a = 0
             b = 0
             # only proceed if at least one contour was found
-            if len(cnts) > 0:
+            if len(contour_num) > 0:
                 # find the largest contour in the mask, then use
                 # it to compute the minimum enclosing circle and
                 # centroid
-                c = max(cnts, key=cv2.contourArea)
+                c = max(contour_num, key=cv2.contourArea)
                 ((x, y), radius) = cv2.minEnclosingCircle(c)
                 a = x
                 b = y
@@ -137,7 +157,6 @@ class Writer:
                         (0, 255, 255), 2)
             
                     cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
 
             width, height = pyautogui.size()
 
@@ -166,7 +185,7 @@ class Writer:
             #cv2.imshow('blurred', blurred)
             #cv2.imshow('hsv', hsv)
             #cv2.imshow('frame',frame)
-            #cv2.imshow('dilate',otsu)
+            #cv2.imshow('dilate',dilate)
             
 
             k=cv2.waitKey(5) & 0xFF
@@ -174,7 +193,6 @@ class Writer:
                 break
 
         self.cap.release()
-
 
 # main function
 mouse = Controller()
